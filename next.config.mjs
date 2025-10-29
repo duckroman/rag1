854732506e9ae4d170c6fb8410ac8e7828ef8716
Jaime Román Carlos <duckroman@gmail.com>
2025-10-29 00:34:36 -0600

    fix: updated next.config.mjs to use ES module syntax

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { isServer }) => {
-    config.resolve.alias['@/'] = require('path').join(__dirname, 'src/');
+    config.resolve.alias['@/'] = path.join(__dirname, 'src/');
     return config;
   },
 };
